a040409c741ecdd6e4d0be728dce4650e7362b67
jaja2302~ <valentinojaja@gmail.com>
2025-08-10 16:29:19 +0700

    Refactor perkiraan_dinamis.php for improved database queries and cleanup: Remove unnecessary aliases for column names, streamline SQL queries, and enhance user instructions in the interface. Update sambung.php to change database connection settings for local development.

--- public/old/sambung.php
+++ public/old/sambung.php
@@ -1,3 +1,3 @@
 <?php
-$link = mysqli_connect("localhost", "fuzzytim_series", "eyhA8kM3", "fuzzytim_series");
-// $link = mysqli_connect("localhost", "root", "", "fts");
+$link = mysqli_connect("localhost", "root", "", "fts");    
+?>
